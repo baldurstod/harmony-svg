eeb569dacb4a9c4bf1837648fc4e2c4b292f6abe
Guillaume <Guillaume>
2023-10-23 21:02:49 +0200

    Added visibility_on, visibility_off

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { closeSVG } from './svg/close.js';
 export { contentCopySVG } from './svg/content_copy.js';
+export { visibilityOffSVG } from './svg/visibility_off.js';
+export { visibilityOnSVG } from './svg/visibility_on.js';
