740a421e479801f57af1c02bb0eaa0e8a94475b2
Guillaume <Guillaume>
2023-12-12 18:53:03 +0100

    Added arrows, rotate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+export { arrowDownwardAltSVG } from './svg/arrow_downward_alt.js';
+export { arrowLeftAltSVG } from './svg/arrow_left_alt.js';
+export { arrowRightAltSVG } from './svg/arrow_right_alt.js';
+export { arrowUpwardAltSVG } from './svg/arrow_upward_alt.js';
 export { checkMarkSVG } from './svg/check_mark.js';
 export { closeSVG } from './svg/close.js';
 export { contentCopySVG } from './svg/content_copy.js';
+export { rotateLeftSVG } from './svg/rotate_left.js';
+export { rotateRightSVG } from './svg/rotate_right.js';
 export { textDecreaseSVG } from './svg/text_decrease.js';
 export { textIncreaseSVG } from './svg/text_increase.js';
 export { visibilityOffSVG } from './svg/visibility_off.js';
