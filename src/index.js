227412df9f0e83533e99fb14181874a104210074
Guillaume <Guillaume>
2023-12-12 14:21:49 +0100

    Added text increase, text decrease

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export { checkMarkSVG } from './svg/check_mark.js';
 export { closeSVG } from './svg/close.js';
 export { contentCopySVG } from './svg/content_copy.js';
+export { textDecreaseSVG } from './svg/text_decrease.js';
+export { textIncreaseSVG } from './svg/text_increase.js';
 export { visibilityOffSVG } from './svg/visibility_off.js';
 export { visibilityOnSVG } from './svg/visibility_on.js';
