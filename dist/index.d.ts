8a05fa3512363895a067c5b27d2d8b994f299f5f
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-31 18:03:43 +0100

    Added rollup config

---
+++ dist/index.d.ts
@@ -0,0 +1,91 @@
+export declare const arrowDownwardAltSVG: string;
+
+export declare const arrowLeftAltSVG: string;
+
+export declare const arrowRightAltSVG: string;
+
+export declare const arrowUpwardAltSVG: string;
+
+export declare const bookmarksPlainSVG: string;
+
+export declare const bugReportSVG: string;
+
+export declare const checkOutlineSVG: string;
+
+export declare const checkSVG: string;
+
+export declare const closeSVG: string;
+
+export declare const contentCopySVG: string;
+
+export declare const cycleSVG: string;
+
+export declare const favoriteSVG: string;
+
+export declare const fireSVG: string;
+
+export declare const lapsSVG: string;
+
+export declare const mailSVG: string;
+
+export declare const manufacturingSVG: string;
+
+export declare const moreHorizSVG: string;
+
+export declare const openInNewSVG: string;
+
+export declare const overscanSVG: string;
+
+export declare const patreonLogoSVG: string;
+
+export declare const pauseSVG: string;
+
+export declare const photoCameraSVG: string;
+
+export declare const playListAddSVG: string;
+
+export declare const playSVG: string;
+
+export declare const print3dSVG: string;
+
+export declare const restartSVG: string;
+
+export declare const rotateLeftSVG: string;
+
+export declare const rotateRightSVG: string;
+
+export declare const runSVG: string;
+
+export declare const sentimentExcitedSVG: string;
+
+export declare const settingsSVG: string;
+
+export declare const sfmLogoSVG: string;
+
+export declare const shareSVG: string;
+
+export declare const shoppingCartSVG: string;
+
+export declare const sortAlphabeticalReverseSVG: string;
+
+export declare const sortAlphabeticalSVG: string;
+
+export declare const sprintSVG: string;
+
+export declare const textDecreaseSVG: string;
+
+export declare const textIncreaseSVG: string;
+
+export declare const viewInArSVG: string;
+
+export declare const visibilityOffSVG: string;
+
+export declare const visibilityOnSVG: string;
+
+export declare const walkSVG: string;
+
+export declare const zoomInSVG: string;
+
+export declare const zoomOutSVG: string;
+
+export { }
